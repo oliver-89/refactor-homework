53a850714ed52bf743767c231a398e29b8034ada
oliver-89 <wjr980729@163.com>
2020-08-28 17:40:26 +0800

    add test Employee case 1. Employee Oliver with type manager

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -1,6 +1,7 @@
 const test = require('ava');
 const { Employee } = require('../src/employee');
 
+
 test('Employee case 1. Employee Oliver with type manager. ', t => {
 
     let employee = new Employee('Oliver','manager');
