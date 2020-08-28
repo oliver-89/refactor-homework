f34818d3882ca3f64bd578a63ab33dfcee89c974
oliver-89 <wjr980729@163.com>
2020-08-28 12:48:55 +0800

    refactor extract function generateOwing

--- src/print.js
+++ src/print.js
@@ -1,22 +1,26 @@
 function printOwing(invoice) {
   
+  
+
+  
+
+  return generateOwing(invoice);
+
+}
+
+module.exports = {
+  printOwing,
+};
+function generateOwing(invoice) {
   let result = '***********************\n**** Customer Owes ****\n***********************\n';
-
   let outstanding = calcOutstanding(invoice);
-
-  // record due date
   recordDueDate(invoice);
-
-  // print details
   result += `name: ${invoice.customer}\n`;
   result += `amount: ${outstanding}\n`;
   result += `amount: ${invoice.dueDate.toLocaleDateString()}`;
   return result;
 }
 
-module.exports = {
-  printOwing,
-};
 function recordDueDate(invoice) {
   const today = new Date();
   invoice.dueDate = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 30);
