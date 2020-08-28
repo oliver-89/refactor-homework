ae0a0de3dfc6f9f17a4e71ce36c4508b7d0fe47e
oliver-89 <wjr980729@163.com>
2020-08-28 18:01:10 +0800

    add test Employee case 2. Employee Oliver with none exist type Oliver.

--- test/employeeTest.js
+++ test/employeeTest.js
@@ -4,9 +4,18 @@ const { Employee } = require('../src/employee');
 
 test('Employee case 1. Employee Oliver with type manager. ', t => {
 
-    let employee = new Employee('Oliver','manager');
+    let employee = new Employee('Oliver', 'manager');
 
     const result = employee.toString();
 
     t.is(result, 'Oliver (manager)');
+});
+
+test('Employee case 2. Employee Oliver with none exist type Oliver. ', t => {
+    try {
+        new Employee('Oliver', 'Oliver');
+    }
+    catch (e) {
+        t.is(e.message, 'Employee cannot be of type Oliver');
+    }
 });
