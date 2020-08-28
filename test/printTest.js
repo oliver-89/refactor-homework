8ab5716d19af7258dad63ba086a80d3d218abbb3
oliver-89 <wjr980729@163.com>
2020-08-28 11:48:28 +0800

    add printOwing case 1. Customer Oliver without amount 1 and 2

--- test/printTest.js
+++ test/printTest.js
@@ -1,8 +1,25 @@
 const test = require('ava');
 const { printOwing } = require('../src/print');
 
+const invoice = {
+    'customer': 'Oliver',
+    'borderSpacing': [{
+        'amount': 1,
+    },
+    {
+        'amount': 2,
+    },]
+};
 
-  
+test('printOwing case 1. Customer Oliver without amount 1 and 2. ', t => {
+
+    const result = printOwing(invoice);
+
+    t.is(result, '');
+});
+
+
+
 
 
 
