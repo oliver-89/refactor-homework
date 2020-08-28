1ec1bfecaa2289023e0fbf3f0c4eabbafb8d8242
oliver-89 <wjr980729@163.com>
2020-08-28 18:07:59 +0800

    refactor extract employee type to globe const

--- src/employee.js
+++ src/employee.js
@@ -1,3 +1,8 @@
+const EMPLOYEE_TYPE = [
+  'engineer',
+  'manager',
+  'salesman',
+];
 class Employee {
   constructor (name, type) {
     this.validateType(type);
@@ -6,11 +11,7 @@ class Employee {
   }
 
   validateType (type) {
-    if (![
-      'engineer',
-      'manager',
-      'salesman',
-    ].includes(type)) {
+    if (!EMPLOYEE_TYPE.includes(type)) {
       throw new Error(`Employee cannot be of type ${type}`);
     }
   }
