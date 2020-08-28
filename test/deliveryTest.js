c220a9cc8b954c0044c82f373f71bc6133c97874
oliver-89 <wjr980729@163.com>
2020-08-28 21:39:47 +0800

    Delivery case 2, is rush true and NY

--- test/deliveryTest.js
+++ test/deliveryTest.js
@@ -12,3 +12,15 @@ test('Delivery case 1, is rush true and MA',t=>{
     let result = deliveryDate(anOrder,isRush);
     t.is(result,3)
 })
+
+test('Delivery case 2, is rush true and NY',t=>{
+    let anOrder = {
+        deliveryState:'NY',
+        placedOn: {
+            plusDays:(num)=>{return num+1}
+        }
+    }
+    let isRush = true;
+    let result = deliveryDate(anOrder,isRush);
+    t.is(result,4)
+})
